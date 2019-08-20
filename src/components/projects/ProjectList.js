afc23572710a204295ceba8ff56ea20200b29fcd
Koblandy <kbaizhigit@outlook.com>
2019-08-20 23:43:41 +0600

    Project summary component

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,25 +1,14 @@
 import React from 'react';
 
+import ProjectSummary from './ProjectSummary';
+
 const ProjectList = () => {
   return (
     <div className="project-list section">
-      <div className="card z-depth-0 project-summary">
-        <span className="card-title">Project Title</span>
-        <p>Posted by John Snow</p>
-        <p className="grey-text">22nd August, 23pm</p>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <span className="card-title">Project Title</span>
-        <p>Posted by Tyrion Lannister</p>
-        <p className="grey-text">22nd August, 23pm</p>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <span className="card-title">Project Title</span>
-        <p>Posted by Dany Targarien</p>
-        <p className="grey-text">22nd August, 23pm</p>
-      </div>
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   );
 };
