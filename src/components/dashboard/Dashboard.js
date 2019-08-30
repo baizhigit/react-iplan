9ac9ede7b3b2f123bf19167c73efc2dfe61e5002
Koblandy <kbaizhigit@outlook.com>
2019-08-31 02:03:16 +0600

    CreateNtification firestore cloud function

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -14,11 +14,13 @@ class Dashboard extends Component {
 
     return (
       <div className="dashboard container">
-        <div className="col s12 m6">
-          <ProjectList projects={projects} />
-        </div>
-        <div className="col s12 m5 offset-m1">
-          <Notifications />
+        <div className="row">
+          <div className="col s12 m6">
+            <ProjectList projects={projects} />
+          </div>
+          <div className="col s12 m5 offset-m1">
+            <Notifications />
+          </div>
         </div>
       </div>
     );
