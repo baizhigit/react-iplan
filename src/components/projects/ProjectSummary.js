287735aecd4296552e017304be5374bcf77271a2
Koblandy <kbaizhigit@outlook.com>
2019-08-30 16:53:08 +0600

    populating real user data in project summary

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -5,7 +5,9 @@ const ProjectSummary = ({ project }) => {
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
         <span className="card-title">{project.title}</span>
-        <p>Posted by John Snow</p>
+        <p>
+          Posted by {project.authorFirstName} {project.authorLastName}
+        </p>
         <p className="grey-text">22nd August, 23pm</p>
       </div>
     </div>
